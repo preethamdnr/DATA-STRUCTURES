3c1db3b9014638b74d064c129bf15a82b91ca4cd
preethamdnr <preethamdongari@gmail.com>
2024-07-14 06:52:19 -0500

    modified Is anagram

--- Is-anagram.js
+++ Is-anagram.js
@@ -1,23 +1,31 @@
 class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
     isAnagram(s, t) {
-        if (s.length !== t.length) {
-            return false;
-        }
-
-        const countS = {};
-        const countT = {};
-
-        for (let i = 0; i < s.length; i++) {
-            countS[s[i]] = 1 + (countS[s[i]] || 0);
-            countT[t[i]] = 1 + (countT[t[i]] || 0);
-        }
-
-        for (const key in countS) {
-            if (countS[key] !== countT[key]) {
-                return false;
+        if(s.length!==t.length) return false;
+        let map={};
+        for(let i=0;i<s.length;i++){
+            let letter=s[i];
+            if(!map[letter]){
+                map[letter]=1;
+            }
+            else{
+                map[letter]++
             }
         }
-
+        for(let i=0;i<t.length;i++){
+            let letter = t[i];
+            if(map[letter]===undefined){
+                return false;
+            }
+            if(map[letter]<1){
+                return false;
+            }
+            map[letter]--;
+        }
         return true;
     }
 }
