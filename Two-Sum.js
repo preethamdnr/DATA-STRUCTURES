60d7fdfa49761d26503b96ed44e4c87aaae95d7b
preethamdnr <preethamdongari@gmail.com>
2024-07-15 17:08:45 -0500

    Two-Sum

---
+++ Two-Sum.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map=new Map();
+        for(let i=0;i<nums.length;i++){
+            let num1=nums[i];
+            let num2 =target-num1;
+            if(map.has(num2)){
+                return[i, map.get(num2)];
+            }
+            map.set(num1, i);
+        }
+    }
+}
